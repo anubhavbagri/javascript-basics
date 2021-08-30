d5d5d5f36ee8b7b3b29342b79663c54aa6db2633
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-31 02:08:40 +0530

    feat: js arrays introduction

---
+++ CFC-MERN-live/lecture-04/arrayIntro.js
@@ -0,0 +1,14 @@
+// let a = [];
+
+// a.push(10);
+// a.push("happy");
+// a.push({ name: "mohit" });
+
+let a = new Array(10); // 10 is the size of the array
+
+a[0] = 99;
+a[18] = "1000";
+
+console.log(a);
+console.log(a[7]);
+console.log(a.length);
