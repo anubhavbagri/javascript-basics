e475ed0662919b74a634d0172249eea6d813ab5e
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-31 02:02:23 +0530

    feat: slice method

---
+++ CFC-MERN-live/lecture-04/sliceEg.js
@@ -0,0 +1,10 @@
+let items = [1, 2, 3, 4, 6, 9];
+
+// console.log(items.slice(1, 3)); //creates sub-array copy
+console.log(items.slice(-3, -1));
+
+let another = items.slice(2, 3);
+
+// another[0] = 100;
+
+// console.log(another);
