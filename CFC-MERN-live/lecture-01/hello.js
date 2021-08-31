f1c4464473a686e3ac756795c99c151c0c7e4cff
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-31 05:50:33 +0530

    feat: types

---
+++ CFC-MERN-live/lecture-01/hello.js
@@ -0,0 +1,21 @@
+console.log("Welcome to CFC");
+
+//can't start variable name with a digit, but $ or _ is possible
+let myName = "Anubhav";
+
+let age = 33;
+let marks = 33.1;
+
+let marks2 = parseInt(33.1);
+
+let isHappy = true; //! true or false
+
+let myChar = "f";
+
+console.log(age);
+console.log(typeof age); //! number
+console.log(typeof marks); //! number
+console.log(marks2);
+console.log(typeof marks2); //! number
+console.log(typeof isHappy); //! boolean
+console.log(typeof myChar); //!string
