df1310ffb87cf4a2762ade13755cc5da271bdd29
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-31 05:51:13 +0530

    feat: expressions

---
+++ CFC-MERN-live/lecture-01/expressions.js
@@ -0,0 +1,10 @@
+let p = 1000;
+let r = 10;
+let t = 3;
+
+// receiver = expression
+let si = (p * r * t) / 100; //arithmetic expression
+let si2 = p * r * t == 100; //conditional expression
+
+console.log(si);
+console.log(si2);
