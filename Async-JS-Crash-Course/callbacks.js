e069b7651331dc6a67231eb5eb3731c4b17ce308
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-11-08 23:57:13 +0530

    refactor: formatting

--- Async-JS-Crash-Course/callbacks.js
+++ Async-JS-Crash-Course/callbacks.js
@@ -3,7 +3,10 @@ const posts = [
     title: "Post One",
     body: "This is post one",
   },
-  { title: "Post Two", body: "This is post two" },
+  {
+    title: "Post Two",
+    body: "This is post two",
+  },
 ];
 
 function getPosts() {
