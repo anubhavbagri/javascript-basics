58381e70bd67dfc9cd902c5fa0135055310a63b5
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-11-08 20:58:15 +0530

    CFC incomplete lectures

---
+++ CFC-MERN-live/lecture-14/formapp/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+      }}
+    >
+      <input></input>
+      <input></input>
+      <input></input>
+      <input></input>
+    </div>
+  );
+}
+
+export default App;
